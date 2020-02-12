33aa1359f753e281a011f818aeab49f307728db1
Volker Buzek <volker.buzek@js-soft.com>
2020-02-12 17:20:28 +0100

    feat(e2e-test): w/ uiveri5
    - upload a file
    - debug config for uiveri5 as local npm module
    - debug & concise notation for uiveri5 tests
    - npm script for e2e test

--- packages/ui5-app/webapp/test/e2e/conf.js
+++ packages/ui5-app/webapp/test/e2e/conf.js
@@ -1,4 +1,21 @@
 exports.config = {
-    profile: "integration",
-    baseUrl: "http://localhost:4081/index.html"
+    /* no profile: "integration" in order to turn the screenshotting off :) */
+    /* instead, copy'pasted https://github.com/SAP/ui5-uiveri5/blob/master/conf/integration.profile.conf.js */
+
+    specResolver: "./resolver/localSpecResolver",
+    pageLoading: {
+        /* used to overcome issues due to pending async work that was started before the waitForUI5 was injected */
+        wait: "3000"
+    },
+    takeScreenshot: {
+        onExpectFailure: false,
+        onExpectSuccess: false,
+        onAction: false
+    },
+    reporters: [ /* none! */],
+
+    // continue w/ standard config
+    // attention: bootstrapping via local UI5 npm module causes UIveri5 to detect a wrong version number 
+    // baseUrl: "http://localhost:4081/index.html" -> reports UI5 0.1.5 :)
+    baseUrl: "http://localhost:4081/index-cdn.html" // bootstrapping from CDN works
 }
